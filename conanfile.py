b7fc2c96189065427883c11b550a8d8c26bd596b
Brian Szmyd <bszmyd@ebay.com>
2026-06-18 09:09:54 -0700

    Ublkp on main channel

--- conanfile.py
+++ conanfile.py
@@ -48,7 +48,7 @@ class HomeBlocksConan(ConanFile):
 
     def build_requirements(self):
         self.test_requires("gtest/[^1.17]")
-        self.test_requires("ublkpp/[^0.32]@oss/dev")
+        self.test_requires("ublkpp/[^0.33]@oss/main")
 
     def requirements(self):
         self.requires("homestore/[^8.0]@oss/dev", transitive_headers=True)
